927b3eafee1325618df9b6763d7b3780e76bbef1
eulerbutcooler <eulerbutcooler@gmail.com>
2025-06-30 14:43:01 +0530

    mugshot changed

--- app/components/Mugshot.tsx
+++ app/components/Mugshot.tsx
@@ -1,21 +1,13 @@
 "use client";
-import { motion } from "motion/react";
-//@ts-ignore
+// import { motion } from "motion/react";
 export default function Mugshot() {
   return (
-    <div className=" h-96 w-72 ml-32">
-      <motion.div
-        whileHover={{
-          boxShadow: "0 0 20px 5px #FFFFFF",
-        }}
-        className="w-full h-full rounded-2xl "
-      >
-        <img
-          src="/mugshot.jpg"
-          alt="amaan pathan"
-          className="object-cover w-full h-full rounded-2xl"
-        ></img>
-      </motion.div>
+    <div className="h-96 w-72 rounded-2xl ml-32 border-3 border-bgdark hover:border-white transition-all duration-150">
+      <img
+        src="/mugshot.jpg"
+        alt="amaan pathan"
+        className="object-cover w-full h-full rounded-2xl"
+      ></img>
     </div>
   );
 }
