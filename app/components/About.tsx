2dc1faa30c5209210ac0ef642d006af0a66d9057
eulerbutcooler <eulerbutcooler@gmail.com>
2025-10-11 14:49:09 +0530

    Updated Resume

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -11,7 +11,7 @@ export default function About() {
         Passionate about scalable backends and performant frontends
       </span>
       <a
-        href="/Resume-July-2025.pdf"
+        href="/Amaan-Pathan-Resume.pdf"
         className="mx-auto my-1 w-48 rounded-full bg-white py-2 text-center text-lg font-extrabold text-black transition-all duration-300 hover:scale-110 hover:shadow-[0_0_20px_5px_white] active:scale-100 sm:mx-0 sm:w-56 sm:py-1 sm:text-xl"
         target="_blank"
         rel="noopener noreferrer"
